d44bf5676f51c971956a6b4cc3429a6034b3c4e3
Kirill <kirill.novikov@gmial.com>
2023-07-18 16:45:25 +0300

    Add https

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -95,7 +95,7 @@
 }
 
 const api = new Api({
-  baseUrl: "http://api.kerbasi.nomoredomains.xyz/",
+  baseUrl: "https://api.kerbasi.nomoredomains.xyz/",
   headers: {
     "Content-Type": "application/json",
   },
