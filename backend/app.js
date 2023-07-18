6c3912cb5edf82e4604b9fa68869f4be5b63bc66
Kirill <kirill.novikov@gmial.com>
2023-07-18 15:01:46 +0300

    Change port to 3000

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const {
 } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 
-const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 app.use(cors());
 
